f7f40e71d68b9438913eca3a49eeddd52e3ec952
henriqueviana <henriquehvisa@gmail.com>
2018-01-03 18:29:36 -0200

    Update TodoForm tests

--- frontend/__tests__/todoForm.spec.js
+++ frontend/__tests__/todoForm.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { expect } from 'chai';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15.4';
 import TodoForm from '../src/todo/todoForm';
 import Grid from '../src/template/grid';
@@ -8,8 +8,13 @@ import Grid from '../src/template/grid';
 Enzyme.configure({adapter: new Adapter()});
 
 describe('<TodoForm />', () => {
+  it('should render yourself without break', () => {
+    const wrapper = shallow(<TodoForm />);
+    expect(wrapper.length).to.equal(1);
+  });
+
   it('should render two <Grid /> components', () => {
     const wrapper = shallow(<TodoForm />);
-    expect(wrapper.find(<Grid />)).to.have.length(2);
+    expect(wrapper.find('.todoForm')).to.be.eql({length: 1});
   });
 });
